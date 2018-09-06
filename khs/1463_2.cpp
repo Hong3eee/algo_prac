4afc208074ed6092aa9f3fe946353a57815f6ee0
Hongseok Kim <jk4986@naver.com>
2018-09-06 18:16:57 +0900

    dp 배열사용

--- khs/1463_2.cpp
+++ khs/1463_2.cpp
@@ -1,37 +1,32 @@
 #include <cstdio>
+#include <iostream>
+#include <queue>
 #include <algorithm>
-#include <cstring>
+
 using namespace std;
 
 int d[1000001];
-
-int solve(int n)
-{
-	int& ret = d[n];
-	if(ret != -1)
-		return ret;
-
-	if(n < 2)
-		return ret = 0;
-
-	int tmp = 987654321;
-	if(n%3 == 0)
-		tmp = min(tmp,solve(n/3));
-
-	if(n%2 == 0)
-		tmp = min(tmp,solve(n/2));
-
-	tmp = min(tmp,solve(n-1));
-
-	return ret = tmp+1;
-}
 
 int main()
 {
 	int n;
 	scanf("%d",&n);
-	memset(d,-1,sizeof(d));
 
-	printf("%d\n",solve(n));
+	for(int i=2;i<=n;i++)
+	{
+		int tmp = 987654321;
+		if(i%3 ==0)
+			tmp = min(tmp,d[i/3]);
+
+		if(i%2 == 0)
+			tmp = min(tmp,d[i/2]);
+
+		tmp = min(tmp,d[i-1]);
+
+		d[i] = tmp+1;
+	}
+
+	printf("%d\n",d[n]);
+
 	return 0;
 }
