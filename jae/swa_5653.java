436634851c3b2f54c1d154401666a548cec9f521
JaeCholJeon <wjswocjf92@gmail.com>
2018-10-11 19:49:20 +0900

    줄기세포
    줄기세포

--- jae/swa_5653.java
+++ jae/swa_5653.java
@@ -9,12 +9,10 @@ public class Solution {
 	int dy[] = {1,0,-1,0};
 	boolean visited[][];
 	ArrayList<node> list;
-	Queue<node>q;
 	Solution(){
 		Scanner sc= new Scanner (System.in);
 		list= new ArrayList();
 		t= sc.nextInt();
-		q= new LinkedList();
 		for(int p = 0 ; p<t; p++) {
 			visited = new boolean[500][500];
 			n = sc.nextInt();
@@ -30,41 +28,39 @@ public class Solution {
 					}
 				}
 			for(int l = 0; l <k;l++) {
-				if(q.isEmpty()) {
 
-					int size= list.size();
-					for(int i = 0 ; i <size;i++) {
-						list.get(i).act--;
-						if(list.get(i).s==1&&list.get(i).act<=0) {
-							list.get(i).s=2;
-							list.get(i).act=list.get(i).n;
-						}
-						else if(list.get(i).s==2&&list.get(i).act>0) { 
-							if(list.get(i).act+1==list.get(i).n)
-								for(int h = 0 ; h<4;h++) {
-									int newy=  list.get(i).i+dy[h];
-									int newx=  list.get(i).j+dx[h];
-									boolean check =false;
-									if(!visited[250+newy][250+newx]) {
-										list.add(new node(newy,newx,list.get(i).n,1));
-										visited[250+newy][250+newx]=true;
-									}
+				int size= list.size();
+				for(int i = 0 ; i <size;i++) {
+					list.get(i).act--;
+					if(list.get(i).s==1&&list.get(i).act<=0) {
+						list.get(i).s=2;
+						list.get(i).act=list.get(i).n;
+					}
+					else if(list.get(i).s==2&&list.get(i).act>0) { 
+						if(list.get(i).act+1==list.get(i).n)
+							for(int h = 0 ; h<4;h++) {
+								int newy=  list.get(i).i+dy[h];
+								int newx=  list.get(i).j+dx[h];
+								boolean check =false;
+								if(!visited[250+newy][250+newx]) {
+									list.add(new node(newy,newx,list.get(i).n,1));
+									visited[250+newy][250+newx]=true;
 								}
-						}
-						else if(list.get(i).s==2&&list.get(i).act<=0) { 
-							if(list.get(i).n==1)
-								for(int h = 0 ; h<4;h++) {
-									int newy=  list.get(i).i+dy[h];
-									int newx=  list.get(i).j+dx[h];
-									boolean check =false;
-									if(!visited[250+newy][250+newx]) {
-										list.add(new node(newy,newx,list.get(i).n,1));
-										visited[250+newy][250+newx]=true;
-									}
+							}
+					}
+					else if(list.get(i).s==2&&list.get(i).act<=0) { 
+						if(list.get(i).n==1)
+							for(int h = 0 ; h<4;h++) {
+								int newy=  list.get(i).i+dy[h];
+								int newx=  list.get(i).j+dx[h];
+								boolean check =false;
+								if(!visited[250+newy][250+newx]) {
+									list.add(new node(newy,newx,list.get(i).n,1));
+									visited[250+newy][250+newx]=true;
+								}
 
-								}
-							list.get(i).s=3;
-						}
+							}
+						list.get(i).s=3;
 					}
 				}
 			}
